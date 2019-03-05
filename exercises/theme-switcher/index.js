d20925fb3deb500ec049a8085b242b435e8d4de7
nalmaghrabi <46870439+nalmaghrabi@users.noreply.github.com>
2019-03-05 18:39:04 +0300

    week6 day2 hw

--- exercises/theme-switcher/index.js
+++ exercises/theme-switcher/index.js
@@ -1,13 +1,26 @@
-// click event for gray button
-
-// click event for white button
 
 function switchGray() {
-    // change background to gray
-    // change font to white
+  let gray= $("#grayButton");
+  let gray2= $("body");
+  let gray3= $("h1");
+  let gray4= $("p");
+  gray.css("background", "gray");
+  gray.css("color", "white");
+  gray2.css("background", "gray");
+  gray3.css("color", "white");
+  gray4.css("color", "white");
 }
+$("#grayButton").on("click",switchGray);
 
 function switchWhite() {
-    // change background to white
-    // change font to black
+  let white= $("#whiteButton");
+  let white2= $("body");
+  let white3= $("h1");
+  let white4= $("p");
+  white.css("background", "white");
+  white.css("color" , "black");
+  white2.css("background", "white");
+  white3.css("color" , "black");
+  white4.css("color" , "black");
 }
+$("#whiteButton").on("click",switchWhite);
